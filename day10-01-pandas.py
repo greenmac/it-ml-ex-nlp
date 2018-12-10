d52c5cbeed3511dbd218a694641e31c2cea9a9f6
greenmac <alwaysmac@msn.com>
2018-12-10 21:56:36 +0800

    day10

---
+++ day10-01-pandas.py
@@ -0,0 +1,23 @@
+import pandas as pd
+
+# df = pd.read_csv("<file_path>") # 讀取csv
+# df = pd.read_json("<file_path>") # 讀取json
+# dfs = pd.read_html("<url or html text>") # 讀取pandas
+
+code_string = """ {'brand_name': nan,
+  'category_name': 'Home/Home Décor/Home Décor Accents',
+  'name': 'Leather Horse Statues',
+  'price': 35,
+  'shipping': 1}"""
+target_dict = eval(code_string)
+
+# df = pd.DataFrame("<dist_variable_name>") # DataFrame 讀取 pandas
+
+# 寫成csv檔
+# df.to_csv('<file_path>', index=False, encoding='utf8') # index不寫上的話，很有可能下次讀檔近來他又幫你重建索引，所以一般我會習慣這樣寫，encoding則是因為中文版windows預設編碼回cp950(真的很討厭)，有一些字彙無法透過這個編碼儲存，像是"喆"或是"Naïve"中間那個有兩個點的i。
+
+# 寫成json檔
+# df.to_json('<file_path>', encoding='utf8')
+
+#轉成python中的dist格式
+# list(df.T.to_dict().values())
---
+++ day11-01-ithome.py
@@ -0,0 +1,37 @@
+import requests
+from bs4 import BeautifulSoup
+import re
+import pandas as pd
+from datetime import datetime
+import matplotlib.pyplot as plt
+
+# 首先我們先到iThome鐵人的首頁，把第一頁到最後一頁的網址抄下來
+url = 'https://ithelp.ithome.com.tw/ironman?page=1#ir-list'
+res = requests.get(url) ## 先爬下第一頁
+soup = BeautifulSoup(res.text, 'lxml')
+maxpage = int(soup.select('.pagination')[0].find_all('a')[-2].text)  ## 定位出最後一頁的頁數print(maxpage)
+
+urls = []
+for i in range(maxpage):  ## 把接下來要爬的網頁準備好
+    page = i+1
+    url = "https://ithelp.ithome.com.tw/ironman?page=" + str(page) + "#ir-list"  ## 大家可以觀察每一頁url的變化
+    urls.append(url)
+
+# 接著爬入每一頁，把每個文章的比賽分類，以及其網址抄下來。之所以要抄分類，是因為文章網址內部找不到分類，所以只好在這邊些記錄下來。
+articles_rows = []
+for idx, url in enumerate(urls):
+    res = requests.get(url)   
+    soup = BeautifulSoup(res.text, 'lxml')
+    articles = soup.select('.ir-list')  ## 找出該頁面的所有文章
+
+    for article in articles:
+        article_dict = {}
+    group = article.select('.group-badge__name')[0].text.replace(' ', '').replace('\n', '')  ## 定位該篇文章參與比賽的參賽組別
+    article_url = article.select('.ir-list__title')[0].select('a')[0]['href']  ## 紀錄該篇文章的網址
+    article_dict['group'] = group  
+    article_dict['article_url'] = article_url
+    articles_rows.append(article_dict)
+    
+    if idx % 10 == 0:  ## 讓你大概知道進度到哪了
+        print(idx)
+# 尚未讀完(從第3項開始)
